fae8fd4d3a12799e36825aa211e82c18ed3b58fb
Victor Bertret <victor.bertret>
2022-12-16 09:19:01 +0100

    Add names and modification on oxygen

--- julia/asm1.jl
+++ julia/asm1.jl
@@ -1,4 +1,4 @@
-using DifferentialEquations, ModelingToolkit, Plots, LaTeXStrings
+using DifferentialEquations, ModelingToolkit, LaTeXStrings
 
 # Define the variables of the system
 X_init = [28.0643, 3.0503, 1532.3, 63.0433, 2245.1, 166.6699, 964.8992, 0.0093, 3.9350, 6.8924, 0.9580, 3.8453, 5.4213]
@@ -7,9 +7,13 @@ X_names = latexstring.(["S_I" "S_S" "X_I" "X_S" "X_{B,H}" "X_{B,A}" "X_P" "S_O" 
 
 # Define the parameters of the system
 @parameters μ_H = 4.0 K_S = 10.0 K_OH = 0.2 K_NO = 0.5 b_H = 0.3 η_g = 0.8 η_h = 0.8 k_h = 3.0 K_X = 0.1 μ_A = 0.5 K_NH = 1.0 b_A = 0.05 K_OA =  0.4 k_a = 0.05 [description = "Kinetic parameters"]
+kinetic_parameters_names = latexstring.(["μ_H" "K_S" "K_{OH}" "K_{NO}" "b_H" "η_g" "η_h" "k_h" "K_X" "μ_A" "K_{NH}" "b_A" "K_{OA}" "k_a"])
 @parameters Y_A = 0.24 Y_H = 0.67 f_P = 0.08 i_XB = 0.08 i_XP = 0.06 [description = "Stoichiometric parameters"]
+stoichiometric_parameters_names = latexstring.(["Y_A" "Y_H" "f_P" "i_{XB}" "i_{XP}"])
 @parameters volume = 1000.0 [description = "Volume of the tank"]
+other_parameters_names = latexstring.(["V"])
 @parameters X_in[1:13] = X_init Q_in=226.0 [description = "Input flow and concentration"]
+input_parameters_names = latexstring.(["S_I^{in}" "S_S^{in}" "X_I^{in}" "X_S^{in}" "X_{B,H}^{in}" "X_{B,A}^{in}" "X_P^{in}" "S_O^{in}" "S_{NO}^{in}" "S_{NH}^{in}" "S_{ND}^{in}" "X_{ND}^{in}" "S_{ALK}^{in}" "Q_{in}"])
 
 # Define our differential: takes the derivative with respect to `t`
 D = Differential(t)
@@ -37,26 +41,13 @@ r = R*process_rates
 eqs = [D(X[i]) ~ (Q_in/volume)*(X_in[i] - X[i]) + r[i] for i in 1:7]
 
 oxygen = 350
-append!(eqs, [D(X[8]) ~ (Q_in/volume)*(X_in[8] - X[8]) + r[8] + oxygen*((t%1 < 0.3) + (t%1 > 0.4)*(t%1 < 0.5) + (t%1 > 0.8)*(t%1 < 0.9)) - 240 * max(X[8] - 8, 0)])
+append!(eqs, [D(X[8]) ~ (Q_in/volume)*(X_in[8] - X[8]) + r[8] + oxygen*((t%1 < 0.3) + (t%1 > 0.4)*(t%1 < 0.5) + (t%1 > 0.8)*(t%1 < 0.9))*(8 - X[8])])
 
 append!(eqs, [D(X[i]) ~ (Q_in/volume)*(X_in[i] - X[i]) + r[i] for i in 9:13])
 
  
 # Bring these pieces together into an ODESystem with independent variable t
-@named ASM1 = ODESystem(eqs,t)
+@named ASM1_sym = ODESystem(eqs,t)
 
 # Symbolically Simplify the System
-simpsys = structural_simplify(ASM1)
-
-# Convert from a symbolic to a numerical problem to simulate
-tspan = (0, 100)
-tsave = LinRange(80, 100, 1000)
-prob = ODEProblem(simpsys, [], tspan)
-
-# Solve the ODE
-@time sol = solve(prob, Tsit5(), reltol = 1e-8, abstol= 1e-8, saveat = tsave)
-
-# Plot the solution
-plot(sol.t, getindex.(sol.u, 9), label = X_names[9])
-plot!(sol.t, getindex.(sol.u, 8) ./2, label=X_names[8], fill=(0, 0.3, :red))
-plot!(sol.t, getindex.(sol.u, 10), label = X_names[10])
+ASM1 = structural_simplify(ASM1_sym)
