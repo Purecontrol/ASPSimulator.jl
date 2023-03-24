cec95404ffe431cc4b874e098350f14598adbf33
Victor Bertret <victor.bertret>
2023-03-24 15:26:01 +0100

    Clean asm1_estimation.jl

--- scripts/model_estimation/asm1_estimation.jl
+++ scripts/model_estimation/asm1_estimation.jl
@@ -1,4 +1,4 @@
-using ASM1Simulator, DifferentialEquations, Statistics, Optimization, OptimizationOptimisers, OptimizationOptimJL, Plots
+using ASM1Simulator, DifferentialEquations, Statistics, Optimization, OptimizationOptimisers, Plots, OptimizationOptimJL
 using StatsPlots, LaTeXStrings, Base.Threads, JLD
 
 ### Define the model ###
@@ -18,7 +18,7 @@ sol_real = solve(ode_prob, Rosenbrock23(), reltol = 1e-8, saveat = tsave, callba
 
 ### Estimate the parameters ###
 function loss(u, p)
-    println(Threads.threadid())
+
     # Estimate parameters
     p_sim = deepcopy(p)
     p_sim[1] = u[1:14]
@@ -33,7 +33,7 @@ function loss(u, p)
     X_init_sim[11:13] = u[27:29]
 
     # Simulate the model
-    sol = solve(remake(ode_prob, tspan=tspan,p=p_sim, u0=X_init_sim), Rosenbrock23(), reltol = 1e-8, saveat = tsave, callback=ASM1Simulator.Models.external_control(tsave_complete, getindex.(sol_real_complete.u, 14)))
+    sol = solve(remake(ode_prob, tspan=tspan,p=p_sim, u0=X_init_sim), Rosenbrock23(), reltol = 1e-8, saveat = tsave, callback=ASM1Simulator.Models.external_control(tsave_complete, getindex.(sol_real_complete.u, 14)), verbose=false)
     
     # Compute the loss
     O2 = getindex.(sol.u, 8)
@@ -50,53 +50,32 @@ bounds = vcat([[i*0.2, i*1.8] for i in vcat(real_p_vec[1:2]...)], [[i*0.2, i*1.8
 lb = [i[1] for i in bounds] ; ub = [i[2] for i in bounds]
 p_init = lb + rand(29).*(ub-lb)
 
-# See initial solution
-p_init_viz = deepcopy(real_p_vec)
-p_init_viz[1] = p_init[1:14]
-p_init_viz[2] = ASM1Simulator.Models.get_stoichiometric_matrix(p_init[15:19])
-p_init_viz[6] = p_init[20]
-p_init_viz[7] = p_init[21]
-X_init_viz = deepcopy(X_init)
-X_init_viz[2] = p_init[22]
-X_init_viz[4:7] = p_init[23:26]
-X_init_viz[11:13] = p_init[27:29]
-# pyplot()
-init_sol = solve(remake(ode_prob, tspan=tspan,p=p_init_viz, u0=X_init_viz), Rosenbrock23(),reltol = 1e-8, saveat = tsave, callback=ASM1Simulator.Models.external_control(tsave_complete, getindex.(sol_real_complete.u, 14)))
-# plot(getindex.(sol_real.u, 9), label="Real")
-# plot!(getindex.(init_sol.u, 9), label="Init solution")
+#################################################################
+################ Solve the optimization problem #################
+#################################################################
 
-# Solve the optimization problem
-lb_optim = lb.*0.2 ; ub_optim = ub.*1.8
-prob = OptimizationProblem(loss, p_init, real_p_vec)
-sol = solve(prob, Optim.ParticleSwarm(lower=lb_optim, upper=ub_optim, n_particles=10), maxiters=100)
+# Define the bounds on the variables
+lb_optim = zeros(29)#lb.*0.2
+ub_optim = [Inf for i in 1:29]#ub.*1.8
+
+# Define the optimizer and the options
+nb_iter = 10
+nb_particules = 4
+optimizer = Optim.ParticleSwarm(lower=lb_optim, upper=ub_optim, n_particles=nb_particules)
+options = Optim.Options(iterations=nb_iter, extended_trace=true, store_trace=true, show_trace=true, show_every=2)
+
+# Solve the problem
+res = optimize(u -> loss(u,real_p_vec), lb_optim, ub_optim, p_init, optimizer, options)
 
 # Get the estimated parameters
 p_estimate = deepcopy(real_p)
-p_estimate[1] = sol.u[1:14]
-p_estimate[2] = sol.u[15:19]
-p_estimate[6] = sol.u[20]
-p_estimate[7] = sol.u[21]
+p_estimate[1] = res.minimizer[1:14]
+p_estimate[2] = res.minimizer[15:19]
+p_estimate[6] = res.minimizer[20]
+p_estimate[7] = res.minimizer[21]
 X_init_estimate = deepcopy(X_init)
-X_init_estimate[2] = sol.u[22]
-X_init_estimate[4:7] = sol.u[23:26]
-X_init_estimate[11:13] = sol.u[27:29]
+X_init_estimate[2] = res.minimizer[22]
+X_init_estimate[4:7] = res.minimizer[23:26]
+X_init_estimate[11:13] = res.minimizer[27:29]
 
-@save "/home/victor/Documents/code/asm1-simulator/data/processed/asm1_estimation/PSO_1.jld" p_estimate X_init_estimate
-data = load("/home/victor/Documents/code/asm1-simulator/data/processed/asm1_estimation/PSO_1.jld")
-p_estimate = data["p_estimate"]
-X_init_estimate = data["X_init_estimate"]
-p_estimate[2] = ASM1Simulator.Models.get_stoichiometric_matrix(p_estimate[2])
-
-### Plot the results ###
-pyplot()
-estimate_sol = solve(remake(ode_prob, tspan=tspan,p=p_estimate, u0=X_init_estimate), Rosenbrock23(),reltol = 1e-8, saveat = tsave, callback=ASM1Simulator.Models.external_control(tsave_complete, getindex.(sol_real_complete.u, 14)))
-plot(getindex.(sol_real.u, 9), label="Real")
-plot!(getindex.(estimate_sol.u, 9), label="Estimated")
-
-p_cin = latexstring.(["μ_H", "K_S", "K_{OH}", "K_{NO}", "b_H", "η_g", "η_h", "k_h", "K_X", "μ_A", "K_{NH}", "b_A", "K_{OA}", "k_a"])
-p_sto = latexstring.(["Y_A", "Y_H", "f_P", "i_{XB}", "i_{XP}"])
-p_oxy = latexstring.(["S_O^{sat}", "K_La"])
-groupedbar([real_p_vec[1] p_estimate[1]], label=["Real" "Estimated"], title="Coefficients cinétiques", xticks=(1:14, p_cin))
-groupedbar([real_p_vec[2] p_test], label=["Real" "Estimated"], title="Coefficients stochiométriques", xticks=(1:5, p_sto))
-groupedbar([[real_p_vec[6], real_p_vec[7]]  Float64.(p_estimate[6:7])], label=["Real" "Estimated"], title="Coefficients oxygènes", xticks=(1:2, p_oxy))
-groupedbar([(X_init)  (X_init_estimate)], label=["Real" "Estimated"], title="Conditions initiales")
+@save "/home/victor/Documents/code/asm1-simulator/data/processed/asm1_estimation/PSO_3.jld" res p_estimate X_init_estimate
